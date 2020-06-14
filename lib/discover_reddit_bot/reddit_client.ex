ccffab9286c68488edf60ca3dbb63535a1dba365
Ironjanowar <ironjanowar@gmail.com>
2020-06-14 23:40:16 +0200

    This closes #4

--- lib/discover_reddit_bot/reddit_client.ex
+++ lib/discover_reddit_bot/reddit_client.ex
@@ -6,7 +6,9 @@ defmodule DiscoverRedditBot.RedditClient do
   plug(Tesla.Middleware.JSON)
 
   def get_comments(url) do
-    case get("#{url}.json") do
+    [url_without_params | _] = String.split(url, "?")
+
+    case get("#{url_without_params}.json") do
       {:ok, %{body: body}} ->
         {:ok, body}
 
